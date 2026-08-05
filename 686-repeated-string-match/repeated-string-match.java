b1fb168ccf495e77eb9337b630c96d2fdef82ede
Piyush Saini <135604471+findpiyush@users.noreply.github.com>
2026-08-05 14:39:40 +0530

    Time: 262 ms (54.74%) | Memory: 43.6 MB (70.85%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int count = 0;
+        StringBuilder temp = new StringBuilder();
+
+        while(temp.length()<b.length())
+        {
+            temp.append(a);
+            count++;
+        }
+        if(temp.toString().contains(b))
+        {
+            return count;
+        }
+        if(temp.append(a).toString().contains(b))
+        {
+            return count+1;
+        }
+        return -1;
+    }
+}
