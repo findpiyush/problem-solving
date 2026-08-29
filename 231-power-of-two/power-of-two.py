e3eb448379b77f33e57e444a641a078e486f5d6e
Piyush Saini <135604471+findpiyush@users.noreply.github.com>
2026-08-29 22:08:16 +0530

    Time: 2 ms (16.98%) | Memory: 19.5 MB (5.36%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,10 +1,11 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n==1: return True
-        i=0
-        while(pow(2,i)<=n):
-            if pow(2,i) == n: return True
-            i+=1
-        
+        if n == 1:
+            return True
+        i = 0
+        while ( pow(2, i) <= n ):  # it grows exponentially so dont do _ in range(0,n+1) or range(0,n//2+1)
+            if pow(2, i) == n:
+                return True
+            i += 1
+
         return False
-        
