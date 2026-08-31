be9f655b770df44f2a2a44d43c2c4a4455bdee38
Piyush Saini <135604471+findpiyush@users.noreply.github.com>
2026-08-31 21:41:27 +0530

    Time: 57 ms (6.44%) | Memory: 29.9 MB (98.29%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -3,6 +3,6 @@ class Solution:
         nums.sort() # O(nlogn)
         for i in range(len(nums)-1): #O(N)
             if nums[i] == nums[i+1]: return True
+        
         return False
-
-    #O(N)
+    # O(NlogN)
